8cf73c989e86b7ad9235456e9cce519fd95cd7d4
Henelllis <hellis1001@gmail.com>
2020-08-08 09:51:08 -0400

    revert ts serverless to yaml for east, introduce aws-sdk into lambda

--- packages/solstice-api/handler.ts
+++ packages/solstice-api/handler.ts
@@ -1,12 +1,27 @@
-import { APIGatewayProxyHandler } from 'aws-lambda';
-import 'source-map-support/register';
+import { APIGatewayProxyHandler } from "aws-lambda";
+import { S3 } from "aws-sdk";
+
+const s3 = new S3(); // Pass in opts to S3 if necessary
+
+var getParams = {
+  Bucket: process.env.BUCKET_NAME, // your bucket name,
+  Key: process.env.FILE_NAME, // path to the object you're looking for
+};
 
 export const hello: APIGatewayProxyHandler = async (event, _context) => {
+  const s3Obj = await s3.getObject(getParams).promise();
+
+  const objectData = s3Obj.Body.toString("utf-8");
+  const parsedData = JSON.parse(objectData);
   return {
     statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless Webpack (Typescript) v1.0! Your function executed successfully!',
-      input: event,
-    }, null, 2),
+    body: JSON.stringify(
+      {
+        message: parsedData,
+        input: event,
+      },
+      null,
+      2
+    ),
   };
-}
+};
