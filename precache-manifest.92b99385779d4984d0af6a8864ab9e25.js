ac60c238e41b3ab8080428e1c8791ae365eb7430
Henelllis <hellis1001@gmail.com>
2020-08-09 14:33:42 -0400

    Updates

---
+++ precache-manifest.92b99385779d4984d0af6a8864ab9e25.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6eda111cdacb396327cc28221beec02e",
+    "url": "/solsticeTables/index.html"
+  },
+  {
+    "revision": "27760db4fdefcca0d7cf",
+    "url": "/solsticeTables/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "338fb635fe62eeec5959",
+    "url": "/solsticeTables/static/js/2.b6d4ac7b.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/solsticeTables/static/js/2.b6d4ac7b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27760db4fdefcca0d7cf",
+    "url": "/solsticeTables/static/js/main.f87b01a9.chunk.js"
+  },
+  {
+    "revision": "afe56aa31f9a5531fb04",
+    "url": "/solsticeTables/static/js/runtime-main.90396404.js"
+  }
+]);
